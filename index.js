28d0b081628cda2aef48933ff2113048839dc685
yanijames3 <jksia.1000@gmail.com>
2022-08-27 00:36:08 +0800

    Added process env for heroku cli

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
